dbca9fb9ff4b2d2f705e90411795550f7d5729ca
josef3113 <33261454+josef3113@users.noreply.github.com>
2022-07-02 15:33:04 +0300

    in main function using at images processor with diff number of worker and measure the time that take.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "time_measure.h"
+#include "images_processor.h"
+#include <vector>
 
 int main() {
 
-	std::cout << "hello world" << std::endl;
+	utilities::TimeMeasur timer;
+
+	std::vector<utilities::TimeMeasur::TimeProcess> times_process;
+
+	int max_num_worker = 9;
+	for (int i = 1; i <= max_num_worker; i++) {
+
+		timer.Start();
+
+		int worker_num = i;
+		some_lib::ImagesProcessor img_processor{ worker_num };
+
+		int image_count = 10;
+		img_processor.ProcessImages(image_count, "empty");
+
+		timer.Stop();
+
+		times_process.push_back(utilities::TimeMeasur::TimeProcess{ i, timer.GetTime().count() });
+
+		timer.Reset();
+	}
+
+
+
+	std::cout << "summary app \n\n" << std::endl;
+
+	for (const auto& time : times_process) {
+
+		int num_worker = std::get<int>(time);
+		auto time_mesure = std::get<std::chrono::nanoseconds>(time);
+
+		std::cout << "num of worker " << num_worker
+			<< "take " << time_mesure.count() << "[nano sec]" << std::endl;
+	}
 }
